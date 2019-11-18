abe7568d699b30608c485b188a25153730b496f8
Danny McClain <dannyxmcclain@gmail.com>
2019-11-18 10:58:30 -0600

    Added width and height options, simplified code

--- code.ts
+++ code.ts
@@ -1,13 +1,27 @@
-let mySelection = figma.currentPage.selection.slice();
+let selectedCommand = figma.command;
+let mySelection = Array.from(figma.currentPage.selection);
 
-mySelection.forEach(function(item) {
-  let itemParent = item.parent;
-  if (itemParent.type === "FRAME") {
-    item.resizeWithoutConstraints(itemParent.width, itemParent.height);
-    item.x = 0;
-    item.y = 0;
-    figma.closePlugin("Done.");
-  } else {
-    figma.closePlugin("No parent frame.");
-  }
-});
+if (mySelection.length > 0) {
+  mySelection.forEach(function(item) {
+    let itemParent = item.parent;
+    if (itemParent.type === "FRAME") {
+      switch (selectedCommand) {
+        case "fitFrame":
+          item.resizeWithoutConstraints(itemParent.width, itemParent.height);
+          item.x = 0;
+          item.y = 0;
+          break;
+        case "fitWidth":
+          item.resizeWithoutConstraints(itemParent.width, item.height);
+          item.x = 0;
+          break;
+        case "fitHeight":
+          item.resizeWithoutConstraints(item.width, itemParent.height);
+          item.y = 0;
+      }
+      figma.closePlugin("✔ Fitted.");
+    } else {
+      figma.closePlugin("⚠️️️ No parent frame.");
+    }
+  });
+} else figma.closePlugin("⚠️ Nothing selected.");
