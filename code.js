e657715dd13a6f145dd99082e69171cff461ab4c
Danny McClain <dannyxmcclain@gmail.com>
2020-03-19 12:51:19 -0500

    added support for groups and components

--- code.js
+++ code.js
@@ -3,9 +3,9 @@ let mySelection = Array.from(figma.currentPage.selection);
 if (mySelection.length > 0) {
     mySelection.forEach(function (item) {
         let itemParent = item.parent;
-        if (itemParent.type === "FRAME") {
+        if (itemParent.type === "FRAME" || itemParent.type === "COMPONENT") {
             switch (selectedCommand) {
-                case "fitFrame":
+                case "fitParent":
                     item.resizeWithoutConstraints(itemParent.width, itemParent.height);
                     item.x = 0;
                     item.y = 0;
@@ -20,8 +20,25 @@ if (mySelection.length > 0) {
             }
             figma.closePlugin("✔ Fitted.");
         }
+        else if (itemParent.type === "GROUP") {
+            switch (selectedCommand) {
+                case "fitParent":
+                    item.resizeWithoutConstraints(itemParent.width, itemParent.height);
+                    item.x = itemParent.x;
+                    item.y = itemParent.y;
+                    break;
+                case "fitWidth":
+                    item.resizeWithoutConstraints(itemParent.width, item.height);
+                    item.x = itemParent.x;
+                    break;
+                case "fitHeight":
+                    item.resizeWithoutConstraints(item.width, itemParent.height);
+                    item.y = itemParent.y;
+            }
+            figma.closePlugin("✔ Fitted.");
+        }
         else {
-            figma.closePlugin("⚠️️️ No parent frame.");
+            figma.closePlugin("⚠️️️ No parent.");
         }
     });
 }
