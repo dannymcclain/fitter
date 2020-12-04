749377807ce424fa3c2b51563b0fa5caca310a6f
Danny McClain <dannyxmcclain@gmail.com>
2020-12-04 11:05:57 -0600

    Updated parentType and messaging
    Added support for Booleans and update success/error UI messages.

--- code.ts
+++ code.ts
@@ -10,37 +10,43 @@ if (mySelection.length > 0) {
           item.resize(itemParent.width, itemParent.height);
           item.x = 0;
           item.y = 0;
+          figma.closePlugin("✓ Fit");
           break;
         case "fitWidth":
           item.resize(itemParent.width, item.height);
           item.x = 0;
+          figma.closePlugin("↔ Fit Width");
           break;
         case "fitHeight":
           item.resize(item.width, itemParent.height);
           item.y = 0;
+          figma.closePlugin("↕ Fit Height");
       }
-      figma.closePlugin("✔ Fitted.");
-    } else if (itemParent.type === "GROUP") {
+    } 
+    // Separate function for groups because X and Y position behave a little different than frames
+    else if (itemParent.type === "GROUP" || itemParent.type === "BOOLEAN_OPERATION") {
       switch (selectedCommand) {
         case "fit":
           item.resize(itemParent.width, itemParent.height);
           item.x = itemParent.x;
           item.y = itemParent.y;
+          figma.closePlugin("✓ Fit");
           break;
         case "fitWidth":
           item.resize(itemParent.width, item.height);
           item.x = itemParent.x;
+          figma.closePlugin("↔ Fit Width");
           break;
         case "fitHeight":
           item.resize(item.width, itemParent.height);
           item.y = itemParent.y;
+          figma.closePlugin("↕ Fit Height");
       }
-      figma.closePlugin("✔ Fitted.");
     } else {
-      if (mySelection.length === 1) {
-        figma.closePlugin("⚠️️️ Can't fit it.");
-      } else if (mySelection.length > 1) {
-        figma.closePlugin("⚠️️️ Can't fit them.");
+      if (itemParent.type === "PAGE") {
+        figma.closePlugin("⚠️️️ Can't fit to a page.");
+      } else {
+        figma.closePlugin("⚠️️️ Didn't fit.");
       }
     }
   });
