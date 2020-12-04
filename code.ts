0309a0168646776b9c79586fe55615d234bdd359
Danny McClain <dannyxmcclain@gmail.com>
2020-12-04 10:29:40 -0600

    Updating name and resizing
    Switched from resizeWithoutConstraints to regular resize. Also update name from Fit to Parent to just Fit.

--- code.ts
+++ code.ts
@@ -6,33 +6,33 @@ if (mySelection.length > 0) {
     let itemParent = item.parent;
     if (itemParent.type === "FRAME" || itemParent.type === "COMPONENT") {
       switch (selectedCommand) {
-        case "fitParent":
-          item.resizeWithoutConstraints(itemParent.width, itemParent.height);
+        case "fit":
+          item.resize(itemParent.width, itemParent.height);
           item.x = 0;
           item.y = 0;
           break;
         case "fitWidth":
-          item.resizeWithoutConstraints(itemParent.width, item.height);
+          item.resize(itemParent.width, item.height);
           item.x = 0;
           break;
         case "fitHeight":
-          item.resizeWithoutConstraints(item.width, itemParent.height);
+          item.resize(item.width, itemParent.height);
           item.y = 0;
       }
       figma.closePlugin("✔ Fitted.");
     } else if (itemParent.type === "GROUP") {
       switch (selectedCommand) {
-        case "fitParent":
-          item.resizeWithoutConstraints(itemParent.width, itemParent.height);
+        case "fit":
+          item.resize(itemParent.width, itemParent.height);
           item.x = itemParent.x;
           item.y = itemParent.y;
           break;
         case "fitWidth":
-          item.resizeWithoutConstraints(itemParent.width, item.height);
+          item.resize(itemParent.width, item.height);
           item.x = itemParent.x;
           break;
         case "fitHeight":
-          item.resizeWithoutConstraints(item.width, itemParent.height);
+          item.resize(item.width, itemParent.height);
           item.y = itemParent.y;
       }
       figma.closePlugin("✔ Fitted.");
