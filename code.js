15fe34ec0328e95e4c7e0ceb4d2850fc56e7723e
Danny McClain <dannyxmcclain@gmail.com>
2020-12-05 10:53:08 -0600

    Added check for locked layers

--- code.js
+++ code.js
@@ -3,47 +3,50 @@ let mySelection = Array.from(figma.currentPage.selection);
 if (mySelection.length > 0) {
     mySelection.forEach(function (item) {
         let itemParent = item.parent;
-        if (itemParent.type === "FRAME" || itemParent.type === "COMPONENT") {
-            switch (selectedCommand) {
-                case "fit":
-                    item.resize(itemParent.width, itemParent.height);
-                    item.x = 0;
-                    item.y = 0;
-                    figma.closePlugin("✓ Fit");
-                    break;
-                case "fitWidth":
-                    item.resize(itemParent.width, item.height);
-                    item.x = 0;
-                    figma.closePlugin("↔ Fit Width");
-                    break;
-                case "fitHeight":
-                    item.resize(item.width, itemParent.height);
-                    item.y = 0;
-                    figma.closePlugin("↕ Fit Height");
-            }
-        }
-        // Separate function for groups because X and Y position behave a little different than frames
-        else if (itemParent.type === "GROUP" || itemParent.type === "BOOLEAN_OPERATION") {
-            switch (selectedCommand) {
-                case "fit":
-                    item.resize(itemParent.width, itemParent.height);
-                    item.x = itemParent.x;
-                    item.y = itemParent.y;
-                    figma.closePlugin("✓ Fit");
-                    break;
-                case "fitWidth":
-                    item.resize(itemParent.width, item.height);
-                    item.x = itemParent.x;
-                    figma.closePlugin("↔ Fit Width");
-                    break;
-                case "fitHeight":
-                    item.resize(item.width, itemParent.height);
-                    item.y = itemParent.y;
-                    figma.closePlugin("↕ Fit Height");
-            }
+        if (item.locked === true) {
+            figma.closePlugin(`🔒 ${item.name} is locked.`);
         }
         else {
-            if (itemParent.type === "PAGE") {
+            if (itemParent.type === "FRAME" || itemParent.type === "COMPONENT") {
+                switch (selectedCommand) {
+                    case "fit":
+                        item.resize(itemParent.width, itemParent.height);
+                        item.x = 0;
+                        item.y = 0;
+                        figma.closePlugin("✅ Fit");
+                        break;
+                    case "fitWidth":
+                        item.resize(itemParent.width, item.height);
+                        item.x = 0;
+                        figma.closePlugin("↔️ Fit Width");
+                        break;
+                    case "fitHeight":
+                        item.resize(item.width, itemParent.height);
+                        item.y = 0;
+                        figma.closePlugin("↕️ Fit Height");
+                }
+            }
+            // Separate function for groups because X and Y position behave a little different than frames
+            else if (itemParent.type === "GROUP" || itemParent.type === "BOOLEAN_OPERATION") {
+                switch (selectedCommand) {
+                    case "fit":
+                        item.resize(itemParent.width, itemParent.height);
+                        item.x = itemParent.x;
+                        item.y = itemParent.y;
+                        figma.closePlugin("✓ Fit");
+                        break;
+                    case "fitWidth":
+                        item.resize(itemParent.width, item.height);
+                        item.x = itemParent.x;
+                        figma.closePlugin("↔ Fit Width");
+                        break;
+                    case "fitHeight":
+                        item.resize(item.width, itemParent.height);
+                        item.y = itemParent.y;
+                        figma.closePlugin("↕ Fit Height");
+                }
+            }
+            else if (itemParent.type === "PAGE") {
                 figma.closePlugin("⚠️️️ Can't fit to a page.");
             }
             else {
--- code.ts
+++ code.ts
@@ -4,23 +4,26 @@ let mySelection = Array.from(figma.currentPage.selection);
 if (mySelection.length > 0) {
   mySelection.forEach(function(item) {
     let itemParent = item.parent;
+    if (item.locked === true){
+      figma.closePlugin(`🔒 ${item.name} is locked.`);
+    } else {
     if (itemParent.type === "FRAME" || itemParent.type === "COMPONENT") {
       switch (selectedCommand) {
         case "fit":
           item.resize(itemParent.width, itemParent.height);
           item.x = 0;
           item.y = 0;
-          figma.closePlugin("✓ Fit");
+          figma.closePlugin("✅ Fit");
           break;
         case "fitWidth":
           item.resize(itemParent.width, item.height);
           item.x = 0;
-          figma.closePlugin("↔ Fit Width");
+          figma.closePlugin("↔️ Fit Width");
           break;
         case "fitHeight":
           item.resize(item.width, itemParent.height);
           item.y = 0;
-          figma.closePlugin("↕ Fit Height");
+          figma.closePlugin("↕️ Fit Height");
       }
     } 
     // Separate function for groups because X and Y position behave a little different than frames
@@ -42,8 +45,8 @@ if (mySelection.length > 0) {
           item.y = itemParent.y;
           figma.closePlugin("↕ Fit Height");
       }
-    } else {
-      if (itemParent.type === "PAGE") {
+    }
+    else if (itemParent.type === "PAGE") {
         figma.closePlugin("⚠️️️ Can't fit to a page.");
       } else {
         figma.closePlugin("⚠️️️ Didn't fit.");
